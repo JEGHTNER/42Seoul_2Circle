4e409971ce596ec53bfd7832db4930e98b1715fa
이제헌 <jeheon0717@naver.com>
2023-02-24 23:53:56 +0900

    <refactor> : function 25 line norminette fix

--- pipex/ft_split_pipex.c
+++ pipex/ft_split_pipex.c
@@ -6,7 +6,7 @@
 /*   By: jehelee <jehelee@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 14:24:19 by jehelee           #+#    #+#             */
-/*   Updated: 2023/02/23 23:21:33 by jehelee          ###   ########.fr       */
+/*   Updated: 2023/02/24 23:52:35 by jehelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,21 +40,21 @@ int	skip_quoted_string(char const *string, int i);
 // 	return (i);
 // }
 
-int	check_sh(char *cmd)
-{
-	int		cmd_len;
-	int		i;
-	char	*tmp;
+// int	check_sh(char *cmd)
+// {
+// 	int		cmd_len;
+// 	int		i;
+// 	char	*tmp;
 
-	cmd_len = ft_strlen(cmd);
-	i = -1;
-	while (++i < 3)
-		cmd_len--;
-	cmd = cmd + cmd_len;
-	if (ft_strncmp(cmd, ".sh", 3) == 0)
-		return (1);
-	return (0);
-}
+// 	cmd_len = ft_strlen(cmd);
+// 	i = -1;
+// 	while (++i < 3)
+// 		cmd_len--;
+// 	cmd = cmd + cmd_len;
+// 	if (ft_strncmp(cmd, ".sh", 3) == 0)
+// 		return (1);
+// 	return (0);
+// }
 
 size_t	ft_strlcpy_pipex(char *dst, const char *src, size_t dstsize)
 {
@@ -80,25 +80,28 @@ size_t	ft_strlcpy_pipex(char *dst, const char *src, size_t dstsize)
 	return (j);
 }
 
+void	ft_strlcpy_quote_check(const char *src, size_t *i, char q, int *q_flag)
+{
+	int		k;
+
+	*q_flag = 0;
+	(*i)++;
+	k = *i;
+	while (src[k] && !*q_flag)
+	{
+		if (src[k] == q)
+			*q_flag = 1;
+		k++;
+	}
+}
+
 size_t	ft_strlcpy_pipex_quote(char *dst, const char *src, size_t *i, size_t *j)
 {
 	char	quote;
 	int		quote_flag;
-	int		k;
-	int		count;
 
 	quote = src[*i];
-	quote_flag = 0;
-	(*i)++;
-	k = *i;
-	count = 0;
-	while (src[k] && !quote_flag)
-	{
-		if (src[k] == quote)
-			quote_flag = 1;
-		k++;
-		count++;
-	}
+	ft_strlcpy_quote_check(src, i, quote, &quote_flag);
 	if (!quote_flag)
 	{
 		dst[*j] = quote;
@@ -160,7 +163,7 @@ int	count_words(char const *string, char seperator)
 
 int	skip_quoted_string(char const *string, int i)
 {
-	int	quote_flag;
+	int		quote_flag;
 	char	quote;
 
 	quote_flag = 0;
@@ -178,20 +181,12 @@ int	skip_quoted_string(char const *string, int i)
 	return (i);
 }
 
-char	*make_string(char const *string, char seperator, int *i)
+int	find_len(char const *string, int len, char seperator)
 {
-	char	*tmp_string;
-	int		len;
-	int		quote_flag;
-	int		tmp_len;
-
-	len = 0;
-	quote_flag = 0;
 	while (string[len])
 	{
 		if (string[len] == '\'' || string[len] == '\"')
 		{
-			quote_flag = 1;
 			len++;
 			while (string[len] && string[len] != string[0])
 				len++;
@@ -201,32 +196,28 @@ char	*make_string(char const *string, char seperator, int *i)
 		else
 			break ;
 	}
-	if (!quote_flag)
-	{
-		while (string[len] && string[len] != seperator)
-			len++;
-		tmp_string = malloc(sizeof(char) * len + 1);
-		if (!tmp_string)
-			return (0);
-		tmp_len = ft_strlcpy_pipex(tmp_string, string, len + 1);
-		if (tmp_len > len)
-			(*i) += tmp_len;
-		else
-			(*i) += len;
-		return (tmp_string);
-	}
+	while (string[len] && string[len] != seperator)
+		len++;
+	return (len);
+}
+
+char	*make_string(char const *string, char seperator, int *i)
+{
+	char	*tmp_string;
+	int		len;
+	int		tmp_len;
+
+	len = 0;
+	len = find_len(string, len, seperator);
+	tmp_string = malloc(sizeof(char) * len + 1);
+	if (!tmp_string)
+		return (0);
+	tmp_len = ft_strlcpy_pipex(tmp_string, string, len + 1);
+	if (tmp_len > len)
+		(*i) += tmp_len;
 	else
-	{
-		tmp_string = malloc(sizeof(char) * len + 1);
-		if (!tmp_string)
-			return (0);
-		tmp_len = ft_strlcpy_pipex(tmp_string, string, len + 1);
-		if (tmp_len > len)
-			(*i) += tmp_len;
-		else
-			(*i) += len;
-		return (tmp_string);
-	}
+		(*i) += len;
+	return (tmp_string);
 }
 
 int	check_awk(char const *string)
@@ -250,34 +241,31 @@ int	check_awk(char const *string)
 
 int check_string(char const *string)
 {
-	int i;
-	int quote_flag;
-	char quote;
-
-	i = 0;
-	quote_flag = 0;
 	if (!check_awk(string))
 		return (NO_AWK);
-	else
-		return (AWK);
-	while (string[i])
+	return (AWK);
+}
+
+char	*quote_line(char const *string, int i, int str_len, char quote)
+{
+	char	*line;
+	int		j;
+
+	while (str_len > 0 && str_len > i)
 	{
-		if (string[i] == '\'' || string[i] == '\"')
-		{
-			quote = string[i];
-			quote_flag = 1;
-			i++;
-			while (string[i] && string[i] != quote)
-				i++;
-			if (string[i])
-				i++;
-		}
-		else
-			i++;
+		if (string[str_len] == quote)
+			break ;
+		str_len--;
 	}
-	if (quote_flag)
-		return (0);
-	return (1);
+	if (!quote)
+	{
+		i = 0;
+		str_len = ft_strlen(string);
+	}
+	line = ft_substr(string, i + 1, str_len - i - 1);
+	if (!line)
+		return (NULL);
+	return (line);
 }
 
 char	*make_quote_line(char const *string)
@@ -299,20 +287,26 @@ char	*make_quote_line(char const *string)
 		}
 		i++;
 	}
-	while (str_len > 0 && str_len > i)
+	return (quote_line(string, i, str_len, quote));
+}
+
+char	*make_awk(const char *string, int *i)
+{
+	char	*line;
+
+	while (string[(*i)] && (*i) < ft_strlen(string) - 3)
 	{
-		if (string[str_len] == quote)
+		if (string[(*i)] == 'a' && string[(*i) + 1] == 'w' \
+		&& string[(*i) + 2] == 'k')
+		{
+			line = ft_strdup("awk");
+			if (!line)
+				return (NULL);
+			(*i) += 3;
 			break ;
-		str_len--;
+		}
+		(*i)++;
 	}
-	if (!quote)
-	{
-		i = 0;
-		str_len = ft_strlen(string);
-	}
-	line = ft_substr(string, i + 1, str_len - i - 1);
-	if (!line)
-		return (0);
 	return (line);
 }
 
@@ -327,19 +321,9 @@ char	**case_awk(const char *string)
 	i = 0;
 	while (ft_isspace(string[i]))
 		i++;
-	while (string[i] && i < ft_strlen(string) - 3)
-	{
-		if (string[i] == 'a' && string[i + 1] == 'w' && string[i + 2] == 'k')
-		{
-			words[0] = ft_strdup("awk");
-			if (!words[0])
-				return (free_all(words));
-			// string[i] = '\0';
-			i += 3;
-			break ;
-		}
-		i++;
-	}
+	words[0] = make_awk(string, &i);
+	if (!words[0])
+		return (free_all(words));
 	words[1] = make_quote_line(&string[i]);
 	if (!words[1])
 		return (free_all(words));
@@ -376,7 +360,7 @@ char	**case_sh(const char *string)
 	return (words);
 }
 
-char	**ft_split_pipex(char const *string, char seperator)
+char **ft_split_norm(char const *string, char seperator)
 {
 	int		i;
 	int		j;
@@ -384,10 +368,6 @@ char	**ft_split_pipex(char const *string, char seperator)
 
 	i = 0;
 	j = 0;
-	if (check_string(string))
-		return (case_awk(string));
-	if (check_sh((char *)string))
-		return (case_sh(string));
 	words = malloc(sizeof(char *) * (count_words(string, seperator) + 1));
 	if (!words)
 		return (0);
@@ -407,6 +387,15 @@ char	**ft_split_pipex(char const *string, char seperator)
 	}
 	words[j] = 0;
 	return (words);
+}
+
+char	**ft_split_pipex(char const *string, char seperator)
+{
+	if (check_string(string))
+		return (case_awk(string));
+	if (check_sh((char *)string))
+		return (case_sh(string));
+	return (ft_split_norm(string, seperator));
 }
 
 // int main()
@@ -433,50 +422,51 @@ char	**ft_split_pipex(char const *string, char seperator)
 // 	free_all(result);
 // }
 
-int main(int argc, char *argv[], char *envp[])
-{
-	int	oldstdout;
-	int	oldstdin;
-	int	fd1;
-	int	fd2;
-	char	buf[1024];
+// int main(int argc, char *argv[], char *envp[])
+// {
+// 	int	oldstdout;
+// 	int	oldstdin;
+// 	int	fd1;
+// 	int	fd2;
+// 	char	buf[1024];
 
-	// fd1 = open("./infile",O_RDONLY);
-    // if (fd1 == -1)
-    // {
-    // 	printf("Error\n");
-    //     return (1);
-    // }
-	// fd2 = open("./outfile", O_WRONLY | O_CREAT | O_TRUNC, 00644);
-    // if (fd2 == -1)
-    // {
-    // 	printf("Error\n");
-    //     return (1);
-    // }
-	// char *args[] = {"usr/bin/awk", "{count++}", "END", "{print count}", NULL}; 
-	// dup2(fd1, 0);
-	// close(fd1);	
-	// // dup2(fd2, 1);
-    // // close(fd2);
-	// execve("usr/bin/awk", args, NULL);
-	char *str = "wc \\\\ -l";
-	char **args2 = ft_split_pipex(str, ' ');
-	//print args2
-	int i = 0;
-	while (args2[i])
-	{
-		printf("%s\n", args2[i]);
-		i++;
-	}
-	fd1 = open("./infile.txt", O_RDONLY);
-	dup2(fd1, STDIN_FILENO);
-	// char *args[] = {"grep Hello", "ls -l", NULL};
-	// execve("/usr/bin/grep", args, envp);
-	//  char *args2[] = {"awk", "{count++} END {printf \"count: %i\", count}", NULL}; 
-	// char *args2[] = {"sh", "no_x_script.sh", NULL};
-	// char *str = "./script\"quote.sh";
+// 	// fd1 = open("./infile",O_RDONLY);
+//     // if (fd1 == -1)
+//     // {
+//     // 	printf("Error\n");
+//     //     return (1);
+//     // }
+// 	// fd2 = open("./outfile", O_WRONLY | O_CREAT | O_TRUNC, 00644);
+//     // if (fd2 == -1)
+//     // {
+//     // 	printf("Error\n");
+//     //     return (1);
+//     // }
+// 	// char *args[] = {"usr/bin/awk", "{count++}", "END", "{print count}", NULL}; 
+// 	// dup2(fd1, 0);
+// 	// close(fd1);	
+// 	// // dup2(fd2, 1);
+//     // // close(fd2);
+// 	// execve("usr/bin/awk", args, NULL);
+// 	// char *str = "wc \\\\ -l";
+// 	char *str = "awk \"{count++} END {printf \\\"count: %i\\\" , count}\"";
+// 	char **args2 = ft_split_pipex(str, ' ');
+// 	//print args2
+// 	int i = 0;
+// 	while (args2[i])
+// 	{
+// 		printf("%s\n", args2[i]);
+// 		i++;
+// 	}
+// 	fd1 = open("./infile.txt", O_RDONLY);
+// 	dup2(fd1, STDIN_FILENO);
+// 	// char *args[] = {"grep Hello", "ls -l", NULL};
+// 	// execve("/usr/bin/grep", args, envp);
+// 	//  char *args2[] = {"awk", "{count++} END {printf \"count: %i\", count}", NULL}; 
+// 	// char *args2[] = {"sh", "no_x_script.sh", NULL};
+// 	// char *str = "./script\"quote.sh";
 	
-	// execve("/usr/bin/awk", args2, envp);
-	execve("/usr/bin/wc", args2, envp);
-	return (0);
-}
+// 	execve("/usr/bin/awk", args2, envp);
+// 	// execve("/usr/bin/wc", args2, envp);
+// 	return (0);
+// }
