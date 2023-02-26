c07e182a620f1748961523916dea65c6b763a01f
이제헌 <jeheon0717@naver.com>
2023-02-26 18:38:37 +0900

    <fix> : add ft_split_pipex function to header

--- pipex/pipex.h
+++ pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: jehelee <jehelee@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 20:16:51 by jehelee           #+#    #+#             */
-/*   Updated: 2023/02/26 00:23:56 by jehelee          ###   ########.fr       */
+/*   Updated: 2023/02/26 18:37:47 by jehelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,12 @@ typedef enum pipex_status
 	PIPE_WRITE,
 }	t_pipex_status;
 
+typedef enum awk
+{
+	NO_AWK,
+	AWK,
+}	t_awk;
+
 typedef struct s_pipex
 {
 	int		fd_infile;
@@ -49,17 +55,48 @@ typedef struct s_pipex
 	pid_t	pid;
 }			t_pipex;
 
+//init_pipex functions
+char		**get_path_args(char *envp[]);
+int			check_sh(char *cmd);
+char		*get_path(char *cmd, char **path_args);
+void		*init_pipex(t_pipex *pipex, char **argv, char *envp[]);
+
 //ft_split_pipex function
-size_t      ft_strlcpy_pipex(char *dst, const char *src, size_t dstsize);
-size_t      ft_strlcpy_quote(char *dst, const char *src, size_t *i, size_t *j);
-int         skip_quoted_string(char const *string, int i);
-char	    **free_2d_arr(char **words);
-int	        count_words(char const *string, char c);
-int	        check_sep(char c, char sep);
-int         skip_quoted_string(char const *string, int i);
-char	    *make_string(char const *string, char seperator, int *i);
-char	    **ft_split_pipex(char const *string, char seperator);
-int         check_sh(char *cmd);
+size_t		ft_strlcpy_pipex(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcpy_quote(char *dst, const char *src, size_t *i, size_t *j);
+void		ft_strlcpy_quote_check(const char *s, size_t *i, char q, int *q_f);
+char		**ft_split_pipex(char const *string, char seperator);
+char		**ft_split_norm(char const *string, char seperator);
+int			check_string(char const *string);
+int			check_awk(char const *string);
+char		*make_awk(const char *string, int *i);
+char		**case_awk(const char *string);
+char		*quote_line(char const *string, int i, int str_len, char quote);
+char		*make_quote_line(char const *string);
+char		**case_sh(const char *string);
+int			check_sep(char c, char sep);
+int			count_words(char const *string, char seperator);
+int			skip_quoted_string(char const *string, int i);
+int			find_len(char const *string, int len, char seperator);
+char		*make_string(char const *string, char seperator, int *i);
+
+//fork function
+void		parent_process(t_pipex *pipex);
+void		child_process(t_pipex *pipex);
+
+//error_utils
+void		error_handle(t_pipex *pipex, char *argv[]);
+void		error_no_cmd(t_pipex *pipex);
+void		error_no_cmd1_path(t_pipex *pipex, char *argv[]);
+void		error_no_cmd2_path(t_pipex *pipex, char *argv[]);
+void		error_no_infile(t_pipex *pipex, char *argv[]);
+void		error_no_pipe(t_pipex *pipex);
+void		error_no_outfile(t_pipex *pipex, char *argv[]);
+
+//free functions
+void		free_pipex(t_pipex *pipex);
+char		**free_2d_arr(char **words);
+
 
 
 #endif
