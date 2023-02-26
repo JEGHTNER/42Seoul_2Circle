a6378518320c7e863afa44bc92c64db11cb35219
이제헌 <jeheon0717@naver.com>
2023-02-26 18:31:54 +0900

    <refactor> : ft_split function refactor

---
+++ pipex/ft_split_pipex_utils2.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split_pipex_utils2.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jehelee <jehelee@student.42.kr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/26 18:19:24 by jehelee           #+#    #+#             */
+/*   Updated: 2023/02/26 18:19:31 by jehelee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"pipex.h"
+
+char	**case_awk(const char *string)
+{
+	char	**words;
+	int		i;
+
+	words = malloc(sizeof(char *) * 3);
+	if (!words)
+		return (0);
+	i = 0;
+	while (ft_isspace(string[i]))
+		i++;
+	words[0] = make_awk(string, &i);
+	if (!words[0])
+		return (free_2d_arr(words));
+	words[1] = make_quote_line(&string[i]);
+	if (!words[1])
+		return (free_2d_arr(words));
+	words[2] = 0;
+	return (words);
+}
+
+char	**case_sh(const char *string)
+{
+	char	**words;
+	int		i;
+
+	words = malloc(sizeof(char *) * 3);
+	if (!words)
+		return (0);
+	i = 0;
+	while (ft_isspace(string[i]))
+		i++;
+	while (string[i] && i < ft_strlen(string) - 2)
+	{
+		if (string[i] == '.' && string[i + 1] == 's' && string[i + 2] == 'h')
+		{
+			words[0] = ft_strdup("sh");
+			if (!words[0])
+				return (free_2d_arr(words));
+			break ;
+		}
+		i++;
+	}
+	words[1] = ft_strdup(string);
+	if (!words[1])
+		return (free_2d_arr(words));
+	words[2] = 0;
+	return (words);
+}
