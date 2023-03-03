93b6d773e40215617c207ea83d53e06dd8f28902
이제헌 <jeheon0717@naver.com>
2023-03-03 23:22:42 +0900

    <feat> : multi_pipe function

--- pipex/bonus/fork_utils_bonus.c
+++ pipex/bonus/fork_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jehelee <jehelee@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 17:07:24 by jehelee           #+#    #+#             */
-/*   Updated: 2023/03/03 18:17:00 by jehelee          ###   ########.fr       */
+/*   Updated: 2023/03/03 23:22:14 by jehelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,5 +107,38 @@ void	last_child_process(t_pipex *pipex, char **argv, int i)
 	{
 		perror("execve_last_child");
 		exit(127);
+	}
+}
+
+void	multi_pipe(t_pipex *pipex, int argc, char *argv[], t_list *pid_list)
+{
+	int		pipe_fd[2];
+	int		i;
+
+	if (pipex->here_doc_flag)
+		i = 2;
+	else
+		i = 1;
+	while (++i < argc - 1)
+	{
+		error_no_pipe(pipex);
+		if (pipe(pipe_fd) == -1)
+			perror(NULL);
+		pipex->pid = fork();
+		if (pipex->pid == -1)
+			perror(NULL);
+		else if (pipex->pid == 0)
+		{
+			if (i == 2 || (pipex->here_doc_flag && i == 3))
+				first_child_process(pipex, argv);
+			if (i == argc - 2)
+				last_child_process(pipex, argv, i);
+			child_process(pipex, argv[i]);
+		}
+		else
+		{
+			ft_lstadd_back((&pid_list), ft_lstnew(pipex->pid));
+			parent_process(pipex);
+		}
 	}
 }
