6d1675e02c68daee7ff97a50a86f7a60926c2967
이제헌 <jeheon0717@naver.com>
2023-03-04 21:23:59 +0900

    <fix> : here_doc bug fix refactor

--- pipex/bonus/fork_utils_bonus.c
+++ pipex/bonus/fork_utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jehelee <jehelee@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 17:07:24 by jehelee           #+#    #+#             */
-/*   Updated: 2023/03/04 13:51:31 by jehelee          ###   ########.fr       */
+/*   Updated: 2023/03/04 21:21:45 by jehelee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,59 @@ void	child_process(t_pipex *pipex, char *argv_i, int pipe_fd[2])
 		exit(127);
 }
 
-void	first_child_process(t_pipex *pipex, int ac, char **av, int pipe_fd[2])
+void	run_single_cmd(t_pipex *pipex, char **av, int i)
+{
+	char	**cmd_args;
+	char	*cmd_path;
+
+	cmd_args = ft_split_pipex(av[i], ' ');
+	cmd_path = get_path(cmd_args[0], pipex->path_args);
+	error_no_cmd_path(cmd_path, av, i);
+	if (dup2(pipex->fd_outfile, STDOUT_FILENO) == -1)
+	{
+		perror("dup2");
+		exit(1);
+	}
+	close(pipex->fd_outfile);
+	if (execve(cmd_path, cmd_args, pipex->envp_args) == -1)
+	{
+		perror("execve_single_cmd");
+		exit(127);
+	}
+}
+
+
+void	run_child_with_input(t_pipex *pipex, int i, int pipe_fd[2], char **av)
+{
+	char **cmd_args;
+	char *cmd_path;
+
+	cmd_args = ft_split_pipex(av[i], ' ');
+	cmd_path = get_path(cmd_args[0], pipex->path_args);
+	error_no_cmd_path(cmd_path, av, i);
+	if (pipex->fd_infile == -1)
+		exit(1);
+	if (dup2(pipex->fd_infile, STDIN_FILENO) == -1)
+	{
+		perror("dup2");
+		exit(1);
+	}
+	close(pipex->fd_infile);
+	if (dup2(pipe_fd[PIPE_WRITE], STDOUT_FILENO) == -1)
+	{
+		perror("dup2");
+		exit(1);
+	}
+	close(pipe_fd[PIPE_WRITE]);
+	if (execve(cmd_path, cmd_args, pipex->envp_args) == -1)
+	{
+		perror("execve_child_with_input");
+		exit(127);
+	}
+}
+
+
+void	first_child_process(t_pipex *pipex, int argc, char **argv, int pipe_fd[2])
 {
 	char	**cmd_args;
 	char	*cmd_path;
@@ -52,20 +104,23 @@ void	first_child_process(t_pipex *pipex, int ac, char **av, int pipe_fd[2])
 
 	close(pipe_fd[PIPE_READ]);
 	if (pipex->here_doc_flag)
-		first_child_heredoc(pipex, ac, av);
-	else
-		first_child_infile(pipex, &i);
-	if (dup2(pipe_fd[PIPE_WRITE], STDOUT_FILENO) == -1)
 	{
-		perror("dup2");
-		exit(1);
+		i = 3;
+		here_doc(pipex->limiter);
+		free(pipex->limiter);
+		if (argc == 5)
+		{
+			cmd_args = ft_split_pipex(argv[i], ' ');
+			cmd_path = get_path(cmd_args[0], pipex->path_args);
+			error_no_cmd_path(cmd_path, argv, i);
+			run_single_cmd(pipex, argv, i);
+		}
 	}
-	cmd_args = ft_split_pipex(av[i], ' ');
-	cmd_path = get_path(cmd_args[0], pipex->path_args);
-	error_no_cmd_path(cmd_path, av, i);
-	close(pipe_fd[PIPE_WRITE]);
-	if (execve(cmd_path, cmd_args, pipex->envp_args) == -1)
-		exit(127);
+	else
+	{
+		i = 2;
+		run_child_with_input(pipex, i, pipe_fd, argv);
+	}
 }
 
 void	last_child_process(t_pipex *pipex, char **argv, int i, int pipe_fd[2])
